3b7130fadad5276ce3c82baa4565ac51890e863f
Yordan Poydovski <Yordan.Poydovski@paysafe.com>
2022-05-11 09:55:10 +0300

    Updating project

---
+++ DogBreeds/Networking/DogBreedsWebServiceTests.swift
@@ -0,0 +1,20 @@
+//
+//  DogBreedsWebServiceTests.swift
+//  DogBreedsTests
+//
+//  Created by Yordan Poydovski on 11.05.22.
+//
+
+@testable import DogBreeds
+import XCTest
+
+final class DogBreedsWebServiceTests: XCTestCase {
+    override func setUp() {
+    }
+
+    func testGetBreedsList() {
+        //	Mocking AF and web service responses is
+        //	not trivial and will take some time
+        XCTAssert(true)
+    }
+}
